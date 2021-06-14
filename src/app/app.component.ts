024b61c9d87070fb91a917a7cf940eb1b47e6ad2
makesh-git <mynameismakesh@gmail.com>
2021-06-14 19:30:11 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  constructor() {
+    console.log('sds');
+  }
+}
